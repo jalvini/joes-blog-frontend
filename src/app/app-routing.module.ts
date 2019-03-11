07f83d830390a085aafa8c8762c50508781c4f52
jalvini <Makeda@2300>
2019-03-10 20:25:38 -0400

    Fixed Login, Added Guard To Routing Module, Updated Readme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {IndexComponent} from './home/index/index.component';
 import {LoginComponent} from './login/login.component';
+import {AuthGuard} from './guards/auth.guard';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: '', component: IndexComponent },
   { path: 'login', component: LoginComponent }
 ];
